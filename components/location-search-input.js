f40068fa801970f21cd8bbf38467bc4e15a121bb
Ruangyot Thanmongkol <ruangyotth@gmail.com>
2019-07-26 18:39:52 +0700

    Fix indent

--- components/location-search-input.js
+++ components/location-search-input.js
@@ -4,48 +4,47 @@ import PlacesAutocomplete from 'react-places-autocomplete'
 import { addCity } from '../actions/'
 
 class LocationSearchInput extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-        city: '' 
+    constructor(props) {
+        super(props)
+        this.state = {
+            city: '' 
+        }
     }
-  }
 
-  handleChange = city => {
-    this.setState({ 
-        city,
-     })
-  };
+    handleChange = city => {
+        this.setState({ 
+            city,
+        })
+    };
 
-  handleSelect = city => {
-    this.props.addCity(city)
-  };
+    handleSelect = city => {
+        this.props.addCity(city)
+    };
 
-  render() {
-    const renderSuggestionList = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
-        <div className="autocomplete-root">
-          <input {...getInputProps()} />
-          <div className="autocomplete-dropdown-container">
-            {suggestions.map(suggestion => (
-              <div {...getSuggestionItemProps(suggestion)}>
-                <span>{suggestion.description}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-    )
-    return (
-      <div>
-        <PlacesAutocomplete
-          value={this.state.city}
-          onChange={this.handleChange}
-          onSelect={this.handleSelect}
-        >
-          {renderSuggestionList}
-        </PlacesAutocomplete>
-      </div>
-    )
-  }
+    render() {
+        const renderSuggestionList = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
+            <div className="autocomplete-root">
+                <input {...getInputProps()} />
+                <div className="autocomplete-dropdown-container">
+                    {suggestions.map(suggestion => (
+                        <div {...getSuggestionItemProps(suggestion)}>
+                            <span>{suggestion.description}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+        return (
+            <div>
+                <PlacesAutocomplete
+                    value={this.state.city}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelect}>
+                    {renderSuggestionList}
+                </PlacesAutocomplete>
+            </div>
+        )
+    }
 }
 const mapDispatchToProps = { addCity }
 
--- reducers/city.js
+++ reducers/city.js
@@ -4,8 +4,8 @@ const cityReducer = (state = {}, action) => {
   switch (action.type) {
       case ADD_CITY_SUCCESS:
           return [
-            ...state, 
-            action.data
+              ...state, 
+              action.data
           ]
       default:
           return state
